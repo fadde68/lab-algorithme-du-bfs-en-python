f3d5fb9427a76548cc118829a7764b9c5ae3f024
fadde68 <54910778+fadde68@users.noreply.github.com>
2020-04-01 14:47:51 -0400

    4

--- b000000000.py
+++ b000000000.py
@@ -19,17 +19,16 @@ eleves["Abdelkrim"]=["Souleyman","Zack","Zoureni"]
 eleves["Souleyman"]=[]
 eleves["Zack"]=[]
 
+def personne_elue(name):
+    return name == 'Zoureni'
+from collections import deque
+
 def search(name):
    search_queue = deque()
    search_queue += eleves[name]
    print( len(search_queue) )
    return False
 
-   def personne_elue(name):
-      from collections import deque
-    return name == 'Zoureni'
 
 if __name__== "__main__":
    search("Boris")
-
-   
