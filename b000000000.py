0be4ea66b206a48323dbf813290e06050e9462cf
fadde68 <akramfadde0@gmail.com>
2020-04-08 12:19:18 -0400

    Tutoriel complété

--- b000000000.py
+++ b000000000.py
@@ -25,13 +25,18 @@ def personne_elue(name):
 from collections import deque
 
 def search(name):
-   search_queue = deque()
-   search_queue += eleves[name]
-   print( len(search_queue) )
-   return False
+    visitees = []
+    search_queue = deque()
+    search_queue += eleves[name]
+    while search_queue:
+       personne = search_queue.popleft()
+       if not personne in visitees:
+          if personne_elue(personne):
+             print(personne + " a le fameux Mac")
+             return True
+          search_queue += eleves[personne]
+          visitees.append(personne)
+    return False
 
 if __name__== "__main__":
-    search("Boris") 
-
-
-
+   search("Boris")
